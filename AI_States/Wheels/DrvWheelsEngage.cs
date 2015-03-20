7d0c8fc468ee0a9f30f5ea0a5da4de3f0c154bc1
klemag <klemag13@nith.no>
2015-03-20 12:34:53 +0100

    Improved robot movement

--- AI_States/Wheels/DrvWheelsEngage.cs
+++ AI_States/Wheels/DrvWheelsEngage.cs
@@ -1,6 +1,12 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using PG4500_2015_Innlevering1.Robocode;
+using Robocode;
 using Santom;
+using System.Drawing;
 
 namespace PG4500_2015_Innlevering1.AI_States
 {
@@ -18,19 +24,41 @@ namespace PG4500_2015_Innlevering1.AI_States
 		// Called once when we transition into this state.
 		public override void EnterState()
 		{
+
 			base.EnterState();
 
 			_targetPosition = Robot.Enemy.Position;
 
-	
-			Robot.SetTurnRight(MathHelpers.normalizeBearing(Robot.Enemy.BearingDegrees));
-			Robot.SetAhead(200);
+			// Approach until it's close enough then line up next to it
+			if (Robot.Enemy.Distance > 200)
+			{
+				Robot.SetTurnRight(MathHelpers.normalizeBearing(Robot.Enemy.BearingDegrees));
+				Robot.SetAhead(Robot.Enemy.Distance - 200);
+			}
+			else
+			{
+				Robot.SetTurnRight(MathHelpers.normalizeBearing(-Robot.Heading + Robot.Enemy.HeadingDegrees));
+				Robot.SetAhead(20);
+			}
 			
 		}
 
 
 		public override string ProcessState()
 		{
+			_targetPosition = Robot.Enemy.Position;
+
+			// Approach until it's close enough then line up next to it
+			if (Robot.Enemy.Distance > 200)
+			{
+				Robot.SetTurnRight(MathHelpers.normalizeBearing(Robot.Enemy.BearingDegrees));
+				Robot.SetAhead(Robot.Enemy.Distance - 200);
+			}
+			else
+			{
+				Robot.SetTurnRight(MathHelpers.normalizeBearing(-Robot.Heading + Robot.Enemy.HeadingDegrees));
+				Robot.SetAhead(20);
+			}
 			string retState = null;
 			if (Robot.DistanceCompleted()) {
 				retState = "Idle";
--- AI_States/Wheels/DrvWheelsIdle.cs
+++ AI_States/Wheels/DrvWheelsIdle.cs
@@ -1,9 +1,18 @@
-﻿using PG4500_2015_Innlevering1.Robocode;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PG4500_2015_Innlevering1.Robocode;
+using Robocode;
+using Santom;
+using System.Drawing;
 
 namespace PG4500_2015_Innlevering1.AI_States
 {
 	public class DrvWheelsIdle : State
 	{
+		private Point2D _targetPosition;
 		public DrvWheelsIdle() : base("Idle")
 		{
 			// Intentionally left blank.
@@ -12,6 +21,7 @@ namespace PG4500_2015_Innlevering1.AI_States
 
 		public override string ProcessState()
 		{
+
 			return null;
 		}
 	}
